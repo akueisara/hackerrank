67b103b8d8a194067e762a697d860d1bfd73fdd7
Kuei <akueisara39@gmail.com>
2016-06-24 17:53:37 -0700

    Add files via upload

--- data-structures/heap/qheap1.java
+++ data-structures/heap/qheap1.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class qheap1 {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
