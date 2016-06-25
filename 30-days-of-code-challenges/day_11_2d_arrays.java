f73fccdf0c773c29c1856fa9903b75fbd3607c29
Kuei <akueisara39@gmail.com>
2016-06-24 17:44:38 -0700

    Add files via upload

--- 30-days-of-code-challenges/day_11_2d_arrays.java
+++ 30-days-of-code-challenges/day_11_2d_arrays.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class day_11_2d_arrays {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
