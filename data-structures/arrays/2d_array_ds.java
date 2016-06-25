bd109423255c4b53cc93b9c089ac2b6529a49f53
Kuei <akueisara39@gmail.com>
2016-06-24 17:52:39 -0700

    Add files via upload

--- data-structures/arrays/2d_array_ds.java
+++ data-structures/arrays/2d_array_ds.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class 2d_array_ds {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
