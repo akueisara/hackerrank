efe7b2589ac6627088cc63dc2e62d5006744afd1
Kuei <akueisara39@gmail.com>
2016-06-24 17:42:05 -0700

    Add files via upload

--- 30-days-of-code-challenges/day_4_class_vs_instance.java
+++ 30-days-of-code-challenges/day_4_class_vs_instance.java
@@ -19,3 +19,32 @@ public class day_4_class_vs_instance {
 			sc.close();
 	    }
 }
+
+class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+        if (initialAge > 0)
+            this.age = initialAge;
+        else {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.age = 0;
+        }
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if (age < 13) 
+            System.out.println("You are young.");
+        else if ( age >=13 && age < 18)
+            System.out.println("You are a teenager.");
+        else
+            System.out.println("You are old.");
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+        this.age = age + 1;
+	}
+}
