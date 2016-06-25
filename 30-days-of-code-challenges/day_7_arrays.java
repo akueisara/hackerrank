348ac40360c0120c60be51827551601aca08baf6
Kuei <akueisara39@gmail.com>
2016-06-24 17:40:33 -0700

    Add files via upload

--- 30-days-of-code-challenges/day_7_arrays.java
+++ 30-days-of-code-challenges/day_7_arrays.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class day_7_arrays {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
