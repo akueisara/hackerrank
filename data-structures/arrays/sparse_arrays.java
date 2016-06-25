eaa26a9db14c65c69f240110a68fe2194ba1c28b
Kuei <akueisara39@gmail.com>
2016-06-24 17:53:21 -0700

    Add files via upload

--- data-structures/arrays/sparse_arrays.java
+++ data-structures/arrays/sparse_arrays.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class sparse_arrays {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
