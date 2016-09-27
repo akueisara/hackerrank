21d2d836913cd5fedc9ce509ee7d9d990d5643cd
Kuei <akueisara39@gmail.com>
2016-09-27 15:32:39 -0700

    fixed bugs

--- data-structures/arrays/left-rotation/Solution.py
+++ data-structures/arrays/left-rotation/Solution.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n, d = map(int, raw_input().split())
 arr = map(int, raw_input().split())
-result = [0] * n;
+result = [0] * n
 for i in range(0,n):
   result[(i+n-d)%n] = arr[i]
 for i in range(0,n):
