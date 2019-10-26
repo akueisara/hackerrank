5a41f5a7d4bc14d67f85bec95f1bcd1455edb409
Kuei <akueisara39@gmail.com>
2019-10-26 18:22:53 +0800

    Update Solution.py

--- 10-days-of-statistics/day-0-mean-median-and-mode/Solution.py
+++ 10-days-of-statistics/day-0-mean-median-and-mode/Solution.py
@@ -1,3 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 def getMean(list):
     sum = 0
     for i in list:
@@ -11,7 +12,7 @@ def getMedian(list):
     copy = list
     copy.sort()
     if(size % 2 == 0):
-        median = float(copy[size/2 - 1] + copy[size/2]) / 2
+        median = float(copy[size//2 - 1] + copy[size//2]) / 2
     else:
         median = copy[(size-1)/2]
     return median
@@ -34,8 +35,50 @@ def getMode(list):
             mode = i
     return mode
 
+size = int(input())
+a = list(map(int, input().split()))
+print(getMean(a))
+print(getMedian(a))
+print(getMode(a))
+ef getMean(list):
+    sum = 0
+    for i in list:
+        sum += i
+    mean = float(sum) / len(list)
+    return mean
+
+def getMedian(list):
+    median = 0.0
+    size = len(list)
+    copy = list
+    copy.sort()
+    if(size % 2 == 0):
+        median = float(copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
+    else:
+        median = copy[(size-1)/2]
+    return median
+
+def getMode(list):
+    mode = 0
+    size = len(list)
+    count, max = 0, 0
+    copy = list
+    copy.sort()
+    current = 0
+    for i in copy:
+        if (i == current):
+            count += 1
+        else:
+            count = 1
+            current = i
+        if (count > max):
+            max = count
+            mode = i
+    return mode
+
+# if it runs in Python 3, change raw_input() to input()
 size = int(raw_input())
 a = list(map(int, raw_input().split()))
-print getMean(a)
-print getMedian(a)
-print getMode(a)
+print(getMean(a))
+print(getMedian(a))
+print(getMode(a))
