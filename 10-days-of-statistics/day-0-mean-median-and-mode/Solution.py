7674604b4b45bf220dcbba94b16d1b4cee1e0a6f
Kuei <akueisara39@gmail.com>
2019-10-26 18:23:42 +0800

    Update Solution.py

--- 10-days-of-statistics/day-0-mean-median-and-mode/Solution.py
+++ 10-days-of-statistics/day-0-mean-median-and-mode/Solution.py
@@ -1,4 +1,5 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 def getMean(list):
     sum = 0
     for i in list:
@@ -13,47 +14,6 @@ def getMedian(list):
     copy.sort()
     if(size % 2 == 0):
         median = float(copy[size//2 - 1] + copy[size//2]) / 2
-    else:
-        median = copy[(size-1)/2]
-    return median
-
-def getMode(list):
-    mode = 0
-    size = len(list)
-    count, max = 0, 0
-    copy = list
-    copy.sort()
-    current = 0
-    for i in copy:
-        if (i == current):
-            count += 1
-        else:
-            count = 1
-            current = i
-        if (count > max):
-            max = count
-            mode = i
-    return mode
-
-size = int(input())
-a = list(map(int, input().split()))
-print(getMean(a))
-print(getMedian(a))
-print(getMode(a))
-ef getMean(list):
-    sum = 0
-    for i in list:
-        sum += i
-    mean = float(sum) / len(list)
-    return mean
-
-def getMedian(list):
-    median = 0.0
-    size = len(list)
-    copy = list
-    copy.sort()
-    if(size % 2 == 0):
-        median = float(copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
     else:
         median = copy[(size-1)/2]
     return median
