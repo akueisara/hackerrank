7dd42a7564bb4aab75bc1cde27918e9d86cc21df
Kuei-Jung Hu <akueisara39@gmail.com>
2020-01-07 22:53:39 +0800

    Add Arrays: Left Rotation

---
+++ interview-preparation-kit/arrays/ctci-array-left-rotation/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int n = a.length;
+        int [] rotated = new int[n];
+        for(int i=0; i<n; i++) {
+            rotated[(i+n-d)%n] = a[i];
+        }
+        return rotated;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
